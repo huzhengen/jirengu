476e9289e71f16ae363a621636cf1942be029664
huzhengen <huzhengen@163.com>
2018-10-19 16:53:25 +0800

    canvas手机端

--- 10-17/README.md
+++ 10-17/README.md
@@ -1,6 +1,14 @@
 # JavaScript
 
-https://wangdoc.com/javascript/index.html
+https://wangdoc.com/javascript/index.html JS自学
+
+https://developer.mozilla.org/zh-CN/docs/Web/API/Canvas_API Canvas自学
+
+语句（statement）是为了完成某种任务而进行的操作
+
+表达式（expression），指一个为了得到返回值的计算式
+
+NaN不等于任何值，包括它本身。NaN在布尔运算时被当作false。
 
 * createElement
 * appendChild
@@ -13,5 +21,8 @@ https://wangdoc.com/javascript/index.html
 * onerror
 * onclick
 * onkeypress
+* onmousedown
+* onmousemove
+* onmouseup
 * localStorage.setItem
 * localStorage.getItem
---
+++ 10-17/canvas-demo/main.js
@@ -0,0 +1,129 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var using = false;
+var eraserEnabled = false;
+
+setCanvasSize(canvas);
+listenToUser(canvas);
+eraser.onclick = function() {
+    eraserEnabled = true;
+    actions.className = 'actions brushActive'
+}
+brush.onclick = function() {
+    eraserEnabled = false;
+    actions.className = 'actions'
+}
+
+function drawCircle(x, y, radius) {
+    ctx.beginPath();
+    ctx.fillStyle = 'black'
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.strokeStyle = 'black';
+    ctx.moveTo(x1, y1);
+    ctx.lineWidth = 5;
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function setCanvasSize(canvas) {
+    getWidthHeight(canvas)
+    window.onresize = function() {
+        getWidthHeight(canvas)
+    }
+}
+
+function getWidthHeight(canvas) {
+    var pageWidth = document.documentElement.clientWidth;
+    var pageHeight = document.documentElement.clientHeight;
+    canvas.width = pageWidth;
+    canvas.height = pageHeight;
+}
+
+function listenToUser(canvas) {
+    var lastPoint = {
+        'x': undefined,
+        'y': undefined
+    };
+
+    //特性检测
+    if (document.body.ontouchstart !== undefined) {
+        //touch
+        canvas.ontouchstart = function(e) {
+            var x = e.touches[0].clientX;
+            var y = e.touches[0].clientY;
+            using = true;
+            if (eraserEnabled) {
+                ctx.clearRect(x - 5, y - 5, 10, 10);
+            } else {
+                lastPoint = {
+                    'x': x,
+                    'y': y
+                };
+            }
+        }
+
+        canvas.ontouchmove = function(e) {
+            var x = e.touches[0].clientX;
+            var y = e.touches[0].clientY;
+            if (!using) return;
+            if (eraserEnabled) {
+                ctx.clearRect(x - 5, y - 5, 10, 10);
+
+            } else {
+                var newPoint = {
+                    'x': x,
+                    'y': y
+                }
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+                lastPoint = newPoint;
+            }
+        }
+        canvas.ontouchend = function(e) {
+            using = false;
+        }
+    } else {
+        canvas.onmousedown = function(e) {
+            var x = e.clientX;
+            var y = e.clientY;
+            using = true;
+            if (eraserEnabled) {
+                ctx.clearRect(x - 5, y - 5, 10, 10);
+            } else {
+                lastPoint = {
+                    'x': x,
+                    'y': y
+                };
+            }
+        }
+
+        canvas.onmousemove = function(e) {
+            var x = e.clientX;
+            var y = e.clientY;
+            if (!using) return;
+            if (eraserEnabled) {
+                ctx.clearRect(x - 5, y - 5, 10, 10);
+
+            } else {
+                var newPoint = {
+                    'x': x,
+                    'y': y
+                }
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+                lastPoint = newPoint;
+            }
+        }
+
+        canvas.onmouseup = function(e) {
+            using = false;
+        }
+    }
+
+
+
+}
