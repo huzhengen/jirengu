fe6a137dddc8c636fabb6a855f3618dfcdcbc1fc
huzhengen <huzhengen@163.com>
2018-11-09 10:31:37 +0800

    jQuery无缝轮播

---
+++ 10-31/main.js
@@ -0,0 +1,46 @@
+let n
+init()
+
+setInterval(() => {
+    makeLeave(getImage(n))
+        .one('transitionend', (e) => {
+            makeEnter($(e.currentTarget))
+        })
+    makeCurrent(getImage(n+1))
+    n++
+}, 3000)
+
+function lengthControl(n) {
+    let len = $('.images>img').length;
+    if (n > len) {
+        n = n % len
+        if (n === 0) {
+            n = len
+        }
+    }
+    return n
+}
+
+function init() {
+    n = 1
+    $(`.images>img:nth-child(${n})`)
+        .addClass('current')
+        .siblings()
+        .addClass('enter')
+}
+
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current')
+}
+
+function makeEnter($node) {
+    return $node.removeClass('current leave').addClass('enter')
+}
+
+function makeLeave($node) {
+    return $node.removeClass('enter current').addClass('leave')
+}
+
+function getImage(n){
+    return $(`.images>img:nth-child(${lengthControl(n)})`)
+}
