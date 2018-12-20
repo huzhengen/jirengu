b584d0a4cf7cb4af524162e508ff9f8f06a38dd3
huzhengen <huzhengen@163.com>
2018-12-20 17:13:06 +0800

    vue Axios

--- 12-12/vue-demo/src/components/HelloWorld.vue
+++ 12-12/vue-demo/src/components/HelloWorld.vue
@@ -2,29 +2,71 @@
   <div class="hello">
     <h1>HelloWorld</h1>
     <h2>{{$route.params.hellomsg}}</h2>
+    <h3>111</h3>
+    <button @click="getData">click to GET</button>
+    <button @click="postData">click to POST</button>
+    <ul>
+      <li v-for="value in topics">{{value.title}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import axios from "axios";
+Vue.prototype.$http = axios;
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
+  props: {},
+  data() {
+    return {
+      topics: []
+    };
+  },
+  methods: {
+    getData() {
+      this.$http
+        .get("https://cnodejs.org/api/v1/topics", {
+          params: {
+            page: 1,
+            limit: 5
+          }
+        })
+        .then(res => {
+          console.log(res.data.data);
+          this.topics = res.data.data;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    getData() {
+      this.$http
+        .post("https://cnodejs.org/api/v1/topics", {
+          params: {
+            page: 1,
+            limit: 5
+          }
+        })
+        .then(res => {
+          console.log(res.data.data);
+          this.topics = res.data.data;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
   list-style-type: none;
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 a {
